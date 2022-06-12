a9f168d483c81985cffce538f53699ec4e9821b1
Chandarala Savanth Chandra Sekhar <81083835+Savanth283@users.noreply.github.com>
2022-06-12 11:13:14 +0530

    Create LinkedListInPy.py

---
+++ LinkedListInPy.py
@@ -0,0 +1,60 @@
+#Inser Operation in LinkedList
+
+class Node:
+  def __init__(self,dataval=None):
+    self.dataval = dataval 
+    self.nextval = None
+
+class Linkedlistp:
+  def __init__(self):
+    self.headval=None
+  
+  def atBegin(self,newdata): #adding a new node at the begin 
+    NewNode = Node(newdata)
+    NewNode.nextval = self.headval 
+    self.headval = NewNode
+
+  def atEnd(self,newdata1):  #adding a new node at the end
+    NewNode1 = Node(newdata1)
+    if self.headval is None:
+      self.headval = NewNode1 
+      return 
+    last =  self.headval 
+    while(last.nextval):
+      last = last.nextval 
+    last.nextval = NewNode1
+  
+  def inBetween(self,newdata2,prevdata):
+    NewNode2 = Node(newdata2)
+    if prevdata is None:
+      print("The given previous node must be present")
+      return 
+    
+    prevdata = self.headval 
+
+    NewNode2.nextval = prevdata.nextval
+    prevdata.nextval = NewNode2
+
+  def listprint1(self):        #prinitng the linked List
+    printval = self.headval
+    while printval is not None:
+      print(printval.dataval)
+      printval = printval.nextval
+ 
+#assigning the values 
+list = Linkedlistp()
+list.headval = Node('23')
+e1 = Node('24')
+e2 = Node('25')
+
+#assigning the data vals 
+list.headval.nextval = e1
+e1.nextval = e2
+
+#cutom inputs 
+list.atBegin("34")
+list.atEnd("45")
+list.inBetween(list.headval.nextval,8)
+
+#printing the list 
+list.listprint1()
